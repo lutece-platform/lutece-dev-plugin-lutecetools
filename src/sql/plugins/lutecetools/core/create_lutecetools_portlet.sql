d078e9e67332a444b79b923e2d709aa630c5dc2e
pierrelevy <pierrelevy@users.noreply.github.com>
2014-07-19 23:20:25 +0000

    LUTECETOOL-3 : Add Multi-threading and optimize cache update
    git-svn-id: http://dev.lutece.paris.fr/svn/lutece/tools/plugin-lutecetools@69873 bab10101-e421-0410-a517-8ce0973de3ef

---
+++ src/sql/plugins/lutecetools/core/create_lutecetools_portlet.sql
@@ -0,0 +1,3 @@
+
+REPLACE INTO core_datastore VALUES ( "core.cache.status.LuteceToolsMavenCacheService.enabled" , "1" );
+REPLACE INTO core_datastore VALUES ( "core.cache.status.LuteceToolsMavenCacheService.timeToLiveSeconds" , "21600" );
