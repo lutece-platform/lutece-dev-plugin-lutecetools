79c058ad70a54df5cc72e0e52fede1c75ba39e51
pierrelevy <pierrelevy@noreply.users.github.com>
2015-02-18 01:41:36 +0100

    LUTECETOOL-10 : Get JIRA infos for components - work in progress

---
+++ src/java/fr/paris/lutece/plugins/lutecetools/service/JiraService.java
@@ -0,0 +1,112 @@
+/*
+ * Copyright (c) 2002-2013, Mairie de Paris
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *  1. Redistributions of source code must retain the above copyright notice
+ *     and the following disclaimer.
+ *
+ *  2. Redistributions in binary form must reproduce the above copyright notice
+ *     and the following disclaimer in the documentation and/or other materials
+ *     provided with the distribution.
+ *
+ *  3. Neither the name of 'Mairie de Paris' nor 'Lutece' nor the names of its
+ *     contributors may be used to endorse or promote products derived from
+ *     this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * License 1.0
+ */
+package fr.paris.lutece.plugins.lutecetools.service;
+
+import com.atlassian.jira.rest.client.api.JiraRestClient;
+import com.atlassian.jira.rest.client.api.domain.Project;
+import com.atlassian.jira.rest.client.api.domain.Version;
+import com.atlassian.jira.rest.client.auth.AnonymousAuthenticationHandler;
+import com.atlassian.jira.rest.client.internal.async.AsynchronousJiraRestClientFactory;
+import fr.paris.lutece.plugins.lutecetools.business.Component;
+import fr.paris.lutece.portal.service.util.AppLogService;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+/**
+ *
+ * @author pierre
+ */
+public class JiraService
+{
+    private static final String URL_JIRA_SERVER = "http://dev.lutece.paris.fr/jira/";
+    private static JiraService _singleton;
+    private static AsynchronousJiraRestClientFactory _factory;
+    private static AnonymousAuthenticationHandler _auth;
+    public static synchronized JiraService instance()
+    {
+        if (_singleton == null)
+        {
+            _singleton = new JiraService();
+            init();
+        }   
+
+        return _singleton;
+    }
+
+    private static void init()
+    {
+        _factory = new AsynchronousJiraRestClientFactory();
+        _auth = new AnonymousAuthenticationHandler();
+    }
+    
+    public void setInfos( Component component )
+    {        
+        JiraRestClient client = null;
+        try
+        {
+            client = _factory.create( new URI(URL_JIRA_SERVER), _auth );
+            Project project = client.getProjectClient().getProject( component.getJiraKey()).claim();
+            Version lastUnreleasedVersion = null;
+            for( Version version : project.getVersions())
+            {
+                if( version.isReleased() )
+                {
+                    lastUnreleasedVersion = version;
+                }
+            }
+            if( lastUnreleasedVersion != null )
+            {
+//                component.setJiraUnreleasedVersion( lastUnreleasedVersion.getName() );
+            }
+        } catch (URISyntaxException ex)
+        {
+            AppLogService.error( "Error using Jira Client API : " + ex.getMessage(), ex);
+        } finally
+        {
+            if( client != null )
+            {
+                try
+                {
+                    client.close();
+                } catch (IOException ex)
+                {
+                    AppLogService.error( "Error using Jira Client API : " + ex.getMessage(), ex);
+                }
+            }
+        }
+   
+    }
+
+}
